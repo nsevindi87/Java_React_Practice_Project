c3f17702cf1aab7982da5ef8c1cdd706cd75ae84
Nizami Sevindi <nsevindi87@gmail.com>
2024-02-16 11:22:23 +0100

    day02

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,41 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        /*
+        ================================= DAY01
+        //VARIABLES
         String newValue = "Hello World";
         int newNumber = 13;
         double dollarValue = 33.3;
         double dollarNewValue = 34.4;
         boolean isEqual = true;
+        System.out.println(newValue);
 
+        //IF CONDITION
         if(dollarValue>dollarNewValue){
             System.out.println("Yükseldi");
         }else{
             System.out.println("düstü");
         }
 
-        System.out.println(newValue);
+        //ARRAYS
+        String[] values ={
+                "Value01",
+                "Value02",
+                "Value03"
+        };
+
+        //FOREACH
+        for(String value: values){
+            System.out.println(value);
+        }
+
+        for(int i =0; i<values.length; i++){
+            System.out.println(values[i]);
+        }
+         */
+
+        //================================= DAY02
 
 
 
