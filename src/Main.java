b88390c9dd0b67b9a8c22dff224f1b48d5b9f4a0
Nizami Sevindi <nsevindi87@gmail.com>
2024-02-16 10:50:06 +0100

    first value

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        String newValue = "Hello World";
+        int newNumber = 13;
+        double dollarValue = 33.3;
+        double dollarNewValue = 34.4;
+        boolean isEqual = true;
+
+        if(dollarValue>dollarNewValue){
+            System.out.println("Yükseldi");
+        }else{
+            System.out.println("düstü");
+        }
+
+        System.out.println(newValue);
+
+
+
+    }
+}
